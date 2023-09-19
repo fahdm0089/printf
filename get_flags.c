a337ddfed2fec00debd9bb71dc5de78d4be71be9
fahdm0089 <fahdm7693@gmail.com>
2023-09-19 12:32:04 -0700

    a

--- get_flags.c
+++ get_flags.c
@@ -23,11 +23,11 @@ int get_flags(const char *format, int *i)
 				flags |= FLAGS_ARR[j];
 				break;
 			}
+
 		if (FLAGS_CH[j] == 0)
 			break;
 	}
-
-	i = curr_i - 1;
+	*i = curr_i - 1;
 
 	return (flags);
 }
